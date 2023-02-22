a42cb718513e7de17a072866e5c645704576773d
Rafik2018 <ismayilovrafik1@gmail.com>
2023-02-22 01:28:22 -0500

    V1

---
+++ src/Class1/LaunchBrowser.java
@@ -0,0 +1,28 @@
+package Class1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        String URL= driver.getCurrentUrl();
+        System.out.println(URL);
+        String title = driver.getTitle();
+        System.out.println("The title of the page is "+title); //   The title of the page is Facebook – log in or sign up
+
+        Thread.sleep(3000);// slow down for 3 seconds
+
+        driver.quit();      // or  driver.close();  will close the one browser
+                            //  driver.quit();      will close whole whole tab
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+/*
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+
+
+        //System.out.println(driver.getCurrentUrl());
+        //System.out.println(driver.getTitle());
+
+        String path = "https://www.google.com/";
+        driver.get(path);
+
+        String URL = driver.getCurrentUrl();
+
+        System.out.println(path);
+        System.out.println(URL);
+
+        String title = driver.getTitle();
+
+        if (path.equalsIgnoreCase(URL)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No Match");
+        }
+        if (title.equalsIgnoreCase("Google")) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No Match");
+        }
+        driver.quit();*/
+
+
+    }
+}
